fe45c5e2785b2e56fc1fe9bd3fa6e23b98d26bf0
Gabriel Forray <gabriel.forray@gmail.com>
2022-02-28 10:17:03 +0100

    Add commentaries

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ from neo4j import GraphDatabase
 import pandas
 
 # Database Credentials
-
 uri             = "bolt://localhost:7687"
 userName        = "neo4j"
 password        = "password"
@@ -11,12 +10,14 @@ password        = "password"
 # Connect to the neo4j database server
 graphDB_Driver  = GraphDatabase.driver(uri, auth=(userName, password))
 
+# Get all nodes in the graph
 def get_all_nodes():
     all_nodes_query = "MATCH (x) RETURN x"
     nodes = graphDB_Driver.session().run(all_nodes_query)
     print(all_nodes_query)
     return nodes
 
+# Get a specific node based on an attribut and its value
 def get_node(attribut, value):
     one_node_query = "MATCH (x {" + attribut +": '" + value + "'}) RETURN x"
     nodes = graphDB_Driver.session().run(one_node_query)
@@ -24,12 +25,14 @@ def get_node(attribut, value):
     for node in nodes:
         return node
 
+# Get all the edges in the graph
 def get_all_edges():
     all_edges_query = "match (x) -[r]-> (y) return r"
     all_edges = graphDB_Driver.session().run(all_edges_query)
     print(all_edges_query)
     return all_edges
 
+# Get a specific edge based on an attribut and its value
 def get_edge(attribut, value, is_str):
     if is_str:
         one_edge_query = "match (x) -[r {" + attribut +": '" + value + "'}]-> (y) return r"
@@ -39,9 +42,9 @@ def get_edge(attribut, value, is_str):
     print(one_edge_query)
     return edge
 
+# Create a node with a name, a type and, optionnally, one or more attributs
 def create_node(name, type, attributs):
     create_query = "CREATE (" + name + ":" + type
-
     if len(attributs) == 1:
         create_query += " {" +attributs[0][0] + ": '" + attributs[0][1] + "'})"
     elif len(attributs) == 0:
@@ -55,6 +58,7 @@ def create_node(name, type, attributs):
     print(create_query)
     return create_query
 
+# Create an edge between two nodes with a connection type and, optionnally, one or more attributs
 def create_edge(start, end, type_connection, attributs):
     create_query = "MATCH (a) WHERE ID(a) = " + str(start) + " MATCH (b) WHERE ID(b) = " + str(end) + " CREATE (a)-[:" + type_connection   
     if len(attributs) == 1:
@@ -71,18 +75,22 @@ def create_edge(start, end, type_connection, attributs):
     print(create_query)
     return create_query
 
+# Delete everything from the graph
 def delete_all_query():
     query = "MATCH (x) -[r]-> (y) DELETE r"
     graphDB_Driver.session().run(query)
     query = "MATCH (a) DELETE a"
     graphDB_Driver.session().run(query)
 
+# Get the id of a node
 def get_id(node):
     return node['x'].id
 
+# Send a request to the server
 def exectute_request(query):
     graphDB_Driver.session().run(query)
 
+# Create the queries to create a link between a city and a departement
 def link_city_departement(city_name, departement_code, number):
     query_match = "MATCH (x" + str(number) +  " {code: '" + departement_code + "'}) MATCH (y" +  str(number) + " {nom: '"+city_name+"'})" 
     query_create = "CREATE (x" + str(number) +  ")-[:Est_dans_le_département]->(y" + str(number) +  ")"
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import api
 import random
 
 df = pandas.read_csv("communes-departement-region.csv")
-#print(df[:100])
 
 # Clear the database
 api.delete_all_query()
@@ -11,15 +10,19 @@ api.delete_all_query()
 # Get the data to make the departments nodes
 data = [df["code_departement"], df["nom_departement"]]
 headers = ["code", "nom"]
+
+# Clean the data to have the departments
 departement = pandas.concat(data, axis=1, keys=headers).drop_duplicates()
 departement = departement[departement['nom'].notna()]
 
-# Create the query to create all the departements nodes
+# Create and execute the query to create all the departements nodes
 query = ""
 for index, row in departement.iterrows():
     query += api.create_node(row['nom'].replace("'", "").replace("-", "").replace(" ", ""),"departement", [["nom", row['nom'].replace("'", " ")],["code", row['code']]]) + " "
 
 api.exectute_request(query)
+
+input("wait")
 
 df = df.drop_duplicates(subset='code_commune_INSEE')
 
@@ -28,6 +31,8 @@ query = ""
 for index, row in df[:1000].iterrows():
     query += api.create_node(row['nom_commune_postal'].replace(" ", ""),"commune", [["nom", row['nom_commune'].replace("'", " ")]])
 api.exectute_request(query)
+
+input("wait")
 
 number = 0
 query_match = ""
@@ -43,10 +48,10 @@ for index, row in df[:1000].iterrows():
     number += 1
     print(number)
 
-query = query_match + query_create
-print(query)
-api.exectute_request(query)
-
-"""end = api.get_id(api.get_node("code", row["code_departement"]))
-start = api.get_id(api.get_node("nom",row['nom_commune'].replace("'", " ")))
-api.exectute_request(api.create_edge(start, end, "Est_dans_le_département",[]))"""
+    if number%100 == 0:
+        query = query_match + query_create
+        print(query)
+        api.exectute_request(query)
+        print("Made the first " + str(number) + " cities")
+        query_match = ""
+        query_create = ""
